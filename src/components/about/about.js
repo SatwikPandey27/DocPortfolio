b33f51bb29a5bff803b08159976f4b0f631174fb
SatwikPandey27 <109542276+SatwikPandey27@users.noreply.github.com>
2024-09-09 20:49:50 +0530

    about section changed

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,11 +9,42 @@ export default function About() {
         <img src={pic} alt="dr nikhil here" className="imgfluid border rounded-circle border-success border-4 imgpic" />
       </div>
       <div className="col-7 mx-auto ms-5 imgtextalign">
-        <p>I am a dedicated and passionate orthopaedic surgeon with a specialization in spine surgery. With over 15 years of experience in the field, I have developed a strong expertise in diagnosing and treating various spinal disorders and conditions.</p>
         <p>
-           My academic achievements include obtaining USMLE certification and fellowships in orthopaedic spine surgery from Sir Ganga Ram Hospital and advanced spine surgery from Macquarie University Hospital and Queen's Medical Center.
-           I've worked as a consultant spine surgeon in leading hospitals in India, including Jaypee Hospital and Indraprastha Apollo Hospitals, and have received the Best FNB Ravi K. Jerath Award.
-           As a skilled and compassionate surgeon, I focus on delivering personalized care to my patients and staying updated with the latest advancements in spine surgery.
+          I am a spine deformity and endoscopy surgeon. I have done FNB spine surgery from Sir Gangaram Hospital, Delhi,  Complex spine fellowship from Macquarie University, Sydney, Australia and Advanced spine fellowship from Queen’s Medical Centre, Nottingham, UK. <br />
+
+          I have worked at various reputed hospitals across Delhi NCR including Jaypee hospital, Noida and Indraprastha Apollo Hospitals, Sarita Vihar. I went abroad for spinal fellowships to make myself at par with the international standards of spinal care.  At present, I am a consultant at Manipal Hospitals Dwarka, Delhi. <br />
+
+          I am expert at all spinal surgeries including occipito- cervical, cervical , thoracic and lumbar spinal surgeries. My forte is adult spinal deformity- ATP/ ALIF and paediatric  scoliosis. I also do endoscopic spine surgeries. <br />
+
+          I have published in both national and international journals. I have presented at both national and international conferences. I was awarded best fellow award by Sir Ganga Ram Hospital, New Delhi. <br />
+
+          I enjoy my field and I strive to provide best possible spinal advice and care. <br />
+
+
+          I treat : <br />
+          <ul>
+            <li>Occipital cervical instability and fractures </li>
+            <li>Cervical spine fractures, disc prolapse, cervical myelopathy </li>
+            <li>Thoracic spine fractures, and disc prolapse </li>
+            <li>Lumbar spine fractures and disc prolapse </li>
+            <li>Sacrum fractures </li>
+            <li>Spinal infections </li>
+            <li>Spinal cord compression </li>
+            <li>Back pain </li>
+            <li>Neck pain </li>
+            <li>Sciatica </li>
+            <li>Scoliosis </li>
+            <li>Kyphosis </li>
+            <li>Spondylo listhesis </li>
+            <li>TB spine </li>
+          </ul>
+
+          <b>Dr Nikhil Jain</b> <br />
+          Spine deformity (scoliosis) & endoscopy surgeon <br />
+          MBBS, MS Ortho, FNB Spine <br />
+          Fellowship Macquarie University Hospital, Sydney, Australia. <br />
+          Fellowship Queen’s Medical Center, Nottingham , UK (Royal college of Surgeons England & Royal College of Surgeons Edinburgh). <br />
+          Call - +91-9910668831
         </p>
       </div>
     </div>
