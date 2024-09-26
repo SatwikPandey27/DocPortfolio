6af800c7281d3348c9a94c7e3766a131978faf68
SatwikPandey27 <109542276+SatwikPandey27@users.noreply.github.com>
2024-09-26 14:16:45 +0530

    Add files via upload

--- src/components/mainpage/page.js
+++ src/components/mainpage/page.js
@@ -3,10 +3,8 @@ import "./page.css";
 import Main from "../../assets/generalphysician.jpg";
 import col1 from "../../assets/scans.jpg";
 import col2 from "../../assets/scans1.jpg";
-// import PfTimeline from "../timeline/timeline";
 import ContactUs from '../contact/contact'
 import Footer from '../footer/footer'
-// import About from '../about/about'
 import logo from '../../assets/logo2.png'
 import pic from '../../assets/img1.jpeg'
 import pic2 from '../../assets/image2.jpeg'
@@ -32,64 +30,47 @@ export default function home() {
                 </p>
               </div>
             </div>
-
-            <div className="row mx-auto text-center px-5 w-75">
-              <div className="img-div col px-auto">
-                <img className="size-image mx-auto p-0" src={pic2} alt="about section 3" />
-                <a className="btn btnhover btn-success mx-auto mt-3" href='/drrajesh'>
-                  <b>Dr. Rajesh Kansal</b> <> </><i className="bi bi-caret-right-fill"></i>
-                </a>
-              </div>
-              <div className="img-div col px-auto">
-                <img className="size-image mx-auto p-0" src={pic3} alt="about section kansal" />
-                <a className="btn btnhover btn-success mx-auto mt-3 w-75 px-0" href='/drkhushboo'>
-                  <b>Dr. Khushboo Kansal</b> <> </><i className="bi bi-caret-right-fill"></i>
-                </a>
-              </div>
-              <div className="img-div col px-auto">
-                <img className="size-image mx-auto p-0" src={pic} alt="about section drnikhil" />
-                <a className="btn btnhover btn-success w-50 mx-auto mt-3" href="/drnikhil">
-                  <b>Dr. Nikhil Jain</b> <> </><i className="bi bi-caret-right-fill"></i>
-                </a>
-              </div>
-            </div>
           </div>
-
           <div className="row">
             <div className="col mx-0 px-0">
               <img className="img-fluid img-main w-100 px-0" loading="lazy" src={Main} alt="" />
             </div>
             <div className="col-4 mx-0 px-0 col-img">
               <div className="row">
-                <img loading="lazy" className="img-fluid img-col2 w-100" src={col2} alt="robotic_surgery" />
+                <img loading="lazy" className="img-fluid w-100" src={col2} alt="robotic_surgery" />
               </div>
               <div className="row">
-                <img loading="lazy" className="img-fluid img-col1 w-100" src={col1} alt="scans" />
+                <img loading="lazy" className="img-fluid w-100" src={col1} alt="scans" />
               </div>
             </div>
           </div>
         </div>
-        {/* <div id="about"></div>
-        <hr className="mx-5 border-2 my-5 border-success-subtle"/>
-        <div>
-          <h1 className="text-center m-5">About Me</h1>
-          <About/>
-        </div> */}
-        {/* <div id="timeline"></div> */}
-        {/* <div>
-          <h1 className="text-center m-5">Timeline</h1>
-          <PfTimeline/>
-          </div> */}
-        <br />
-        <br />
+        <div className="row mx-auto text-center px-5 justify-content-around">
+          <div className="img-div1 col-12 col-md-4">
+            <img className="size-image mx-auto" src={pic2} alt="about section 3" />
+            <a className="btn btnhover btn-success mx-auto mt-3" href='/drrajesh'>
+              <b>Dr. Rajesh Kansal</b> <> </><i className="bi bi-caret-right-fill"></i>
+            </a>
+          </div>
+          <div className="img-div2 col-12 col-md-4">
+            <img className="size-image mx-auto" src={pic3} alt="about section kansal" />
+            <a className="btn btnhover btn-success mx-auto mt-3" href='/drkhushboo'>
+              <b>Dr. Khushboo Kansal</b> <> </><i className="bi bi-caret-right-fill"></i>
+            </a>
+          </div>
+          <div className="img-div3 col-12 col-md-4">
+            <img className="size-image mx-auto" src={pic} alt="about section drnikhil" />
+            <a className="btn btnhover btn-success mx-auto mt-3" href="/drnikhil">
+              <b>Dr. Nikhil Jain</b> <> </><i className="bi bi-caret-right-fill"></i>
+            </a>
+          </div>
+        </div>
         <div id="contact"></div>
         <br />
-        <br />
-        <br />
-        {/* <hr className="mx-5 border-2 my-5 border-success-subtle"/> */}
         <hr className="mx-5 border-2 mb-5 border-success-subtle"/>
         <div>
           <h1 className="text-center">Contact Me</h1>
+          <br />
           <ContactUs/>
         </div>
         <div id="footer">
