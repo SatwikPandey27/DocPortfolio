96c0ffe44e8b8936e84896ce8741243703860667
SatwikPandey27 <109542276+SatwikPandey27@users.noreply.github.com>
2024-10-22 16:52:43 +0530

    Update page.js

--- src/components/mainpage/page.js
+++ src/components/mainpage/page.js
@@ -1,45 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./page.css";
-// import Main from "../../assets/generalphysician.jpg";
-// import col1 from "../../assets/scans.jpg";
-// import col2 from "../../assets/scans1.jpg";
-// import ContactUs from '../contact/contact'
 import ReviewCarousel from '../testimonials/testimonials'
 import Footer from '../footer/footer'
 import Treatment from "../treatment/treatment";
 import AboutUs from "../about/aboutus";
 import Faq from "../faq/faq";
-// import logo from '../../assets/logo2.png'
-import pic from '../../assets/img1.jpeg'
-import slide1 from '../../assets/slide1.png'
-import slide2 from '../../assets/slide2.png'
-import slide3 from '../../assets/slide3.png'
-import slide4 from '../../assets/slide4.png'
-import pic2 from '../../assets/image2.jpeg'
-import pic3 from '../../assets/image3.jpeg'
+import pic from '../../assets/img1.jpeg';
+import slide1 from '../../assets/slide1.png';
+import slide2 from '../../assets/slide2.png';
+import slide3 from '../../assets/slide3.png';
+import slide4 from '../../assets/slide4.png';
+import pic2 from '../../assets/image2.jpeg';
+import pic3 from '../../assets/image3.jpeg';
 
-export default function home() {
+export default function Home() {
+  useEffect(() => {
+    const myCarousel = document.querySelector('#carouselExampleAutoplaying');
+    new window.bootstrap.Carousel(myCarousel, {
+      interval: 3000,
+      pause: false,
+    });
+  }, []);
+  
   return (
     <>
-    <div className="home">
-      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel" data-bs-pause="false" data-bs-interval="3000">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src={slide4} class="d-block w-100" alt="..."/>
-          </div>
-          <div class="carousel-item">
-            <img className="img-size" src={slide1} class="d-block w-100" alt="..."/>
-          </div>
-          <div class="carousel-item">
-            <img className="img-size" src={slide2} class="d-block w-100" alt="..."/>
-          </div>
-          <div class="carousel-item">
-            <img className="img-size" src={slide3} class="d-block w-100" alt="..."/>
+      <div className="home">
+        <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" data-bs-pause="false" data-bs-interval="3000">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={slide4} className="d-block w-100" alt="Carousel Slide 4" />
+            </div>
+            <div className="carousel-item">
+              <img className="img-size d-block w-100" src={slide1} alt="Carousel Slide 1" />
+            </div>
+            <div className="carousel-item">
+              <img className="img-size d-block w-100" src={slide2} alt="Carousel Slide 2" />
+            </div>
+            <div className="carousel-item">
+              <img className="img-size d-block w-100" src={slide3} alt="Carousel Slide 3" />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div id="about"></div>
+        <div id="about"></div>
         <br />
         <br />
         <hr className="mx-5 border-2 mb-5 border-success-subtle"/>
@@ -49,7 +52,7 @@ export default function home() {
           <AboutUs/>
         </div>
 
-      <div id="treatment"></div>
+        <div id="treatment"></div>
         <br />
         <br />
         <hr className="mx-5 border-2 mb-5 border-success-subtle"/>
@@ -69,44 +72,42 @@ export default function home() {
           <br />
           <br />
           <br />
-          <div class="card img-div1 col-13 col-md-4">
-            <img src={pic2} class="card-img-top pt-2" alt="about section 3"/>
-            <div class="card-body">
-              <h5 class="card-title">Dr. Rajesh Kansal</h5>
-              <p class="card-text px-0">General Physician <br />
-              MBBS
-              <br /> 40 years of experience in general medicine</p>
+          <div className="card img-div1 col-13 col-md-4">
+            <img src={pic2} className="card-img-top pt-2" alt="Dr. Rajesh Kansal"/>
+            <div className="card-body">
+              <h5 className="card-title">Dr. Rajesh Kansal</h5>
+              <p className="card-text px-0">General Physician <br />
+                MBBS <br /> 40 years of experience in general medicine</p>
               <br />
-              <br />
-              <br />
-              <a href="/drrajesh" class="btn btn-primary">Know more <i className="bi bi-caret-right-fill"></i></a>
+              <a href="/drrajesh" className="btn btn-primary">Know more <i className="bi bi-caret-right-fill"></i></a>
             </div>
           </div>
-          <div class="card img-div1 col-13 col-md-4">
-            <img src={pic3} class="card-img-top pt-2" alt="about section 3"/>
-            <div class="card-body">
-              <h5 class="card-title">Dr. Khushboo Kansal</h5>
-              <p class="card-text px-0">
+          <div className="card img-div1 col-13 col-md-4">
+            <img src={pic3} className="card-img-top pt-2" alt="Dr. Khushboo Kansal"/>
+            <div className="card-body">
+              <h5 className="card-title">Dr. Khushboo Kansal</h5>
+              <p className="card-text px-0">
                 Consultant Integrative Psychiatrist <br />
                 Lead in Perinatal Mental Health Services <br />
                 MBBS, MD Psychiatry, MRC Psych (UK), PMH-C</p>
-              <a href="http://www.linkedin.com/in/khushboo-kansal-14749252" className="bi-linkedin socials-specific"> </a>
-                <br />
-              <a href="/drkhushboo" class="btn btn-primary">Know more <i className="bi bi-caret-right-fill"></i></a>
+              <a href="http://www.linkedin.com/in/khushboo-kansal-14749252" target="_blank" rel="noopener noreferrer" className="bi-linkedin socials-specific"> </a>
+              <br />
+              <a href="/drkhushboo" className="btn btn-primary">Know more <i className="bi bi-caret-right-fill"></i></a>
             </div>
           </div>
-          <div class="card img-div3 col-13 col-md-4">
-            <img src={pic} class="card-img-top pt-2" alt="about section 3"/>
-            <div class="card-body">
-              <h5 class="card-title">Dr. Nikhil Jain</h5>
-              <p class="card-text px-0">MBBS <br /> MS Ortho, FNB Spine Surgery <br /> Fellowship MUH Hospital, Sydney, Australia <br /> Fellowship QMC, Nottingham, UK</p>
-              <a href="https://www.linkedin.com/company/drnikhiljain" className="bi-linkedin socials-specific"> </a>
-              <a href="https://www.youtube.com/@Dr.NikhilJain" className="bi-youtube socials-specific"> </a>
+          <div className="card img-div3 col-13 col-md-4">
+            <img src={pic} className="card-img-top pt-2" alt="Dr. Nikhil Jain"/>
+            <div className="card-body">
+              <h5 className="card-title">Dr. Nikhil Jain</h5>
+              <p className="card-text px-0">MBBS <br /> MS Ortho, FNB Spine Surgery <br /> Fellowship MUH Hospital, Sydney, Australia <br /> Fellowship QMC, Nottingham, UK</p>
+              <a href="https://www.linkedin.com/company/drnikhiljain" target="_blank" rel="noopener noreferrer" className="bi-linkedin socials-specific"> </a>
+              <a href="https://www.youtube.com/@Dr.NikhilJain" target="_blank" rel="noopener noreferrer" className="bi-youtube socials-specific"> </a>
               <br />
-              <a href="/drnikhil" class="btn btn-primary">Know more <i className="bi bi-caret-right-fill"></i></a>
+              <a href="/drnikhil" className="btn btn-primary">Know more <i className="bi bi-caret-right-fill"></i></a>
             </div>
           </div>
         </div>
+
         <div id="testimonials"></div>
         <br />
         <br />
@@ -116,15 +117,7 @@ export default function home() {
           <br />
           <ReviewCarousel/>
         </div>
-        {/* <div id="contact"></div> */}
-        {/* <br /> */}
-        {/* <br /> */}
-        {/* <hr className="mx-5 border-2 mb-5 border-success-subtle"/> */}
-        {/* <div> */}
-          {/* <h1 className="text-center">Contact Us</h1> */}
-          {/* <br /> */}
-          {/* <ContactUs/> */}
-        {/* </div> */}
+
         <div id="faq"></div>
         <br />
         <br />
@@ -138,14 +131,15 @@ export default function home() {
         <div id="footer">
           <Footer />
         </div>
+
         <div className="floating-buttons">
-        <a href="tel:+91 88105 60977" className="call-button">
-          <i className="bi bi-telephone-fill"></i>
-        </a>
-        <a href="https://wa.me/8810560977" target="_blank" rel="noopener noreferrer">
-          <i className="bi bi-whatsapp"></i>
-        </a>
-      </div>
+          <a href="tel:+91 88105 60977" className="call-button">
+            <i className="bi bi-telephone-fill"></i>
+          </a>
+          <a href="https://wa.me/8810560977" target="_blank" rel="noopener noreferrer">
+            <i className="bi bi-whatsapp"></i>
+          </a>
+        </div>
       </div>
     </>
   );
